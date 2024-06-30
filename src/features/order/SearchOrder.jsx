f2e8e5061a13869d783d9c03406434b26260d541
YousefTarek <youseftarek.business@gmail.com>
2024-06-30 08:21:09 +0300

    edit: layout

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -18,7 +18,7 @@ function SearchOrder() {
         placeholder="Search Order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className=" w-30 rounded-full px-4 py-2 text-sm bg-yellow-100 sm:focus:w-72 transition-all duration-300 placeholder:text-stone-400 w-40 sm:w-64 focus:outline-none focus:ring focus:ring-yellow-500"
+        className="rounded-full px-4 py-2 text-sm bg-yellow-100 sm:focus:w-72 transition-all duration-300 placeholder:text-stone-400 w-36 sm:w-64 focus:outline-none focus:ring focus:ring-yellow-500"
       />
     </form>
   );
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,15 +4,28 @@ import UserName from "../features/user/UserName";
 
 function Header() {
   return (
-    <header className="flex items-center justify-between bg-yellow-400 uppercase px-4 sm:px-6 py-3 border-b border-stone-300">
-      <Link
-        to="/"
-        className="tracking-widest text-sm sm:text-base md:sm:text-lg"
-      >
-        Fast React Pizza Co.
-      </Link>
-      <SearchOrder />
-      <UserName />
+    <header className="flex flex-col">
+      <div className="px-2 flex items-center justify-between text-stone-200 bg-stone-800">
+        <p className="text-sm font-light">
+          Made By:<span className="font-medium">Yousef Tarek</span>
+        </p>
+        <a
+          className="underline text-sm"
+          href="https://github.com/youseftrek/react-food-delivery"
+        >
+          Github repo &rarr;
+        </a>
+      </div>
+      <div className="flex items-center justify-between bg-yellow-400 uppercase px-4 sm:px-6 py-3 border-b border-stone-300">
+        <Link
+          to="/"
+          className="tracking-widest text-sm sm:text-base md:sm:text-lg"
+        >
+          Fast React Pizza Co.
+        </Link>
+        <SearchOrder />
+        <UserName />
+      </div>
     </header>
   );
 }
